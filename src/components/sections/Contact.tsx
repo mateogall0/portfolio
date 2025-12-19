019fb3312100adb04da96bc6d9bc8d289394d644
mateogall0 <mateogesede@gmail.com>
2025-12-19 14:17:45 -0300

    feat: contact with icons

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,3 +1,7 @@
+import { MdEmail } from "react-icons/md";
+import { FiDownload } from "react-icons/fi";
+
+
 export default function Contact() {
   return (
     <section
@@ -7,13 +11,18 @@ export default function Contact() {
       <h3 className="text-2xl md:text-2xl font-bold text-center">
         Get in touch with me
       </h3>
-      <div className="grid grid-cols-1 gap-8 items-stretch">
+      <div className="grid grid-cols-2 gap-8 items-stretch text-center">
         <a
           href="/cv_en.pdf"
           download="Mateo Gallo CV.pdf"
-          className="inline-block rounded-lg bg-black px-4 py-2 text-white"
+          className="inline-flex items-center gap-2 rounded-lg bg-black px-4 py-2 text-white border border-gray-700"
         >
-          Download CV
+          <FiDownload/> Download CV
+        </a>
+        <a href="mailto:mateogesede@gmail.com"
+          className="inline-flex items-center gap-2 rounded-lg bg-black px-4 py-2 text-white border border-gray-700"
+        >
+          <MdEmail/>Send me an email
         </a>
       </div>
     </section>
